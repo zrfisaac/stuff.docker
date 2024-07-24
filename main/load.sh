4a09785f05d33ed966e8aade336935e36c063301
Isaac Caires <zrfisaac@gmail.com>
2024-07-24 06:16:15 -0300

    zrfisaac.stuff.docker.main : 0.0.1

---
+++ main/load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+mkdir -p mssql
+mkdir -p public
+mkdir -p webapps
+$sudo docker compose up -d
