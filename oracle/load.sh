393703812dac0fc35c24aebf1a3dc38eb7ee3cda
Isaac Caires <zrfisaac@gmail.com>
2024-07-25 17:43:16 -0300

    zrfisaac.stuff.docker.oracle : 0.0.1

---
+++ oracle/load.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker run -d -p 1521:1521 --name oracle --network bridge oracle
