eb7e95e778b1ca1ed0b09594505a99ad9808a9e9
Isaac Caires <zrfisaac@gmail.com>
2024-07-25 17:43:23 -0300

    zrfisaac.stuff.docker.mssql : 0.0.1

---
+++ mssql/shell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker exec -it mssql bash
---
+++ mssql/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ shell ]
+# - : docker login
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker build -t mssql .
+$sudo docker tag mssql zrfisaac/mssql:latest
+$sudo docker login
+$sudo docker push zrfisaac/mssql:latest
